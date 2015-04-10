e6333c6947cc4bbe87c828e2532af59ea1556a06
Wille Marcel <wille@wille.blog.br>
2015-04-10 15:18:00 -0300

    fix setup.cfg

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from codecs import open as codecs_open
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 # Get the long description from the relevant file
@@ -8,7 +8,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 
 
 setup(name='lc8_download',
-      version='0.1',
+      version='0.1.1',
       description="Library to download Landsat 8 imagery from Amazon servers",
       long_description=long_description,
       classifiers=[
